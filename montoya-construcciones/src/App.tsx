39f99918741cb493b6a352e3a8a421e0767c8a0a
Ariel200609 <arielmontoya200609@gmail.com>
2026-02-22 22:58:11 -0300

    fix: correccion de mayusculASl

--- montoya-construcciones/src/App.tsx
+++ montoya-construcciones/src/App.tsx
@@ -1,7 +1,6 @@
 import './index.css';
 import Hero from './components/Hero';
 import Obras from './components/Obras';
-import Navbar from './components/Navbar';
 import Especialidades from './components/Especialidades';
 import Footer from './components/Footer';
 import WhatsAppButton from './components/WhatsAppButton';
@@ -9,7 +8,6 @@ import WhatsAppButton from './components/WhatsAppButton';
 function App() {
   return (
     <main className="font-sans selection:bg-stone-900 selection:text-[#F5F5F0]">
-      <Navbar />
       <Hero />
       <Obras />
       <Especialidades />
